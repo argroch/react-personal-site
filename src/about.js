b6cf35b3f0759d04bee07d93e0ff32c3405ba47d
Aaron Groch <ar.groch@gmail.com>
2019-06-26 22:44:39 +0200

    finally figured out I needed an array map instead of a for loop

--- src/about.js
+++ src/about.js
@@ -4,6 +4,17 @@ import './index.css';
 
 class AboutMePage extends React.Component {
 	render () {
-		return ()
+		return (
+				<div className="container">
+					<MyHeader />
+					<div className="row">
+						<div className="col-md-6 offset-md-3 center-it">
+							<p>
+								{"Hi! I'm full-stack web developer, with experience in teaching adults to code. And so forth and so forth..."}
+							</p>
+						</div>
+					</div>
+				</div>
+			)
 	}
 }
--- src/header.js
+++ src/header.js
@@ -1,22 +1,38 @@
 import React from 'react';
 import './index.css';
 
-const navTitles = {
-	names: ['About Me','Projects','Resume','Contact'], 
-	labels: ['about','projects','resume','current','contact'] 
-};
-var navItems = [];
-for(var i = 0; i < navTitles['names'].length; i++) {
-	navItems.push(<li className="nav-items"><a className="nav-link" href="#" id={navTitles['labels'][i]}>{navTitles['names'][i]}</a></li>);
-}
+
 
 export class MyHeader extends React.Component {
+	niceness(x) {
+		console.log(x + " dance!");
+	}
+
 	render() {
+
+		// const navTitles = {
+		// 	names: ['About Me','Projects','Resume','Contact'], 
+		// 	labels: ['about','projects','resume','contact'],
+		// };
+		// var navItems = [];
+		// for(var i = 0; i < navTitles['names'].length; i++) {
+		// 	var label = navTitles['labels'][i];
+		// 	navItems.push(<li className="nav-items"><span className="nav-link" id={navTitles['labels'][i]} value={label} onClick={ () => {this.niceness(label)}}>{navTitles['names'][i]}</span></li>);
+		// }
+
+		var names = ['About Me','Projects','Resume','Contact'];
+		var labels = ['about','projects','resume','contact'];
+		var navItems = [];
+		names.map((name,index) => {
+			navItems.push(<li className="nav-items"><span className="nav-link" id={labels[index]} onClick={ () => {this.niceness(labels[index])}}>{name}</span></li>);
+		});
+		
+
 		return (
 			<div className="row">
-				<div className="col-md-6 offset-md-3 center-it">
+				<div className="col-md-6 offset-md-3 center-it" key="thisthing">
 					<p>
-						<img src={require("./pinataaron.jpg")} id="my_pic" />
+						<img src={require("./pinataaron.jpg")} alt="it me!" id="my_pic" onClick={ () => {this.niceness("hiya!")}} />
 					</p>
 					<h1>
 						Aaron Groch
